dc13a6e1d9831510d38b7a7b0910a3482f9b397f
Sooryakrishna M <sk2004international@gmail.com>
2024-09-19 23:21:25 +0530

    added the first task update

--- adventer-game.py
+++ adventer-game.py
@@ -1,9 +1,9 @@
 name= input("Enter your name ").upper()
 print("Welcome",name,"to the Adventure Game \n")
-print("You have entered a Jungle and you have to make strong decisions to not to 'DIE'")
+print("You have entered a Jungle and you have to make strong decisions to not to 'DIE' \n")
 
 
-answer = input("You have come to a bridge .Do you want to cross it or stay there and try fishing for a while?\n Type Cross to cross the bridge or Fishing to fish ").lower()
+answer = input("You have come to a bridge .Do you want to cross it or stay there and try fishing for a while?\n Type Cross to cross the bridge or Fishing to fish \n").lower()
 
 if answer=="cross":
     print("You didnt think about the need of food in a Jungle ,thus this means you lost at the beginning due to lack of critical thinking") 
